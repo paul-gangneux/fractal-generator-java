8ffbd2fe34a6c46fe97b88416cf80a47803c1ce5
Olivier Moreau <m242@pm.me>
2021-12-01 23:58:23 +0100

    Julia complete

--- src/Julia.java
+++ src/Julia.java
@@ -1,25 +1,45 @@
-public class Julia implements TwoDoublesToInt {
-	private static int MAX = 1000;
-	private static int MIN = 0;
-	private static int radius = 2;
+import java.util.function.Function;
 
-	public Complex f(Complex z) {
-		// TODO
-		return z.increment();
+// A main to test this class
+// ImageGenerator ig = new ImageGenerator();
+//
+// Function<Complex,Complex> f = z -> z.multiply(z).add(new Complex(-0.729, 0.1889));
+// TwoDoublesToInt func = Julia.JuliaFactory(1000, 0, 2, f);
+// ig.create(func, "images/image.png");
+ 
+
+public class Julia implements TwoDoublesToInt {
+	private int max = 1000;
+	private int min = 0;
+	private int radius = 2;
+	private Function<Complex,Complex> func
+		= z -> z.multiply(z).add(new Complex(-0.729, 0.1889));
+
+	public static Julia JuliaFactory(
+		int max, int min, int radius, Function<Complex,Complex> f) {
+		Julia x = new Julia();
+		x.max = max;
+		x.min = min;
+		x.radius = radius;
+		x.func = f;
+
+		return x;
 	}
 
+	@Override
 	public int doublesToInt(double x, double y) {
 		int res = 0;
 		Complex z = new Complex(x, y);
-		// TODO
-		while (res < MAX-1 && z.abs() <= radius) {
-			z = f(z);
+		while (res < max-1 && z.abs() <= radius) {
+			z = func.apply(z);
 			res++;
 		}
 		return res;
 	}
 
-	public int maxValue() { return MAX; }
-	public int minValue() { return MIN; }
+	@Override
+	public int maxValue() { return max; }
 
+	@Override
+	public int minValue() { return min; }
 }
