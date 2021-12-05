e2452aee60bd4fd19e7bdba66c8781e699157ce1
Paul Gangneux <paul.v.gangneux@gmail.com>
2021-12-05 20:45:08 +0100

    modulation possible du taux d'anti-crénelge et de la fonction de calcul des couleurs, changements dans les setteurs et restructuration interne à ImagagGenerator

--- src/ImageGenerator.java
+++ src/ImageGenerator.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
+import java.util.function.Function;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -14,15 +15,18 @@ public class ImageGenerator {
     private double shiftX, shiftY;
     private TwoDoublesToInt function;
     private BufferedImage image;
+    private int aa_amount; // taux d'anti-crénelage
 
     private double x1,y1;
     private double x2,y2;
 
-    private double delta;
+    private double step;
 
     private int threshold; // pour le multithreading
 
-    private boolean antiAliasing;
+    private boolean antiAliasing; // vrai si l'anti-crénelage est actif
+
+    private ThreeIntToInt valueToColor; 
 
     private class MainWork extends RecursiveAction {
 
@@ -31,27 +35,12 @@ public class ImageGenerator {
         private double ox;
         private double oy;
 
-        /*
-        private int valueToColor(int v) {
-            if (v>255 || v<0) return 0xff0000; //cas erreur
-            if (v==255) return 0;
-            return rgbToInt(255-v, 255-v, v);
-            
-        }
-        */
-
         public MainWork(int hmin, int hmax, double ox, double oy) {
             this.hmin = hmin;
             this.hmax = hmax;
             this.ox = ox;
             this.oy = oy;
-        }
-
-        private int valueToColor(int v, int min, int max) {
-            if (v<min || v>max) return 0x000000; //cas erreur
-            if (v==max) return 0;
-            return Color.HSBtoRGB((float)(v+min)/max, 0.8f, 0.7f);
-        }     
+        }   
 
         @Override
         protected void compute() {
@@ -63,17 +52,17 @@ public class ImageGenerator {
                 int min = function.minValue();
                 int max = function.maxValue();
                 double x = ox;
-                double yReset = oy + hmin*delta*zoom;
+                double yReset = oy + hmin*step*zoom;
                 double y = yReset;
 
                 for (int i=0; i<width; i++) {
                     for (int j=hmin; j<hmax; j++) {
                         int val = function.doublesToInt(x,y);
-                        int col = valueToColor(val, min, max);
+                        int col = valueToColor.apply(val, min, max);
                         image.setRGB(i,j,col);
-                        y += delta*zoom;
+                        y += step*zoom;
                     }
-                    x += delta*zoom;
+                    x += step*zoom;
                     y = yReset;
                 }
             }
@@ -100,6 +89,7 @@ public class ImageGenerator {
             } else {
                 for (int i = 0; i < width; i++) {
                     for (int j = hmin; j < hmax; j++) {
+                        /*
                         int p1, p2, p3, p4;
                         p1 = image.getRGB(i*2, j*2);
                         p2 = image.getRGB(i*2+1, j*2);
@@ -109,6 +99,24 @@ public class ImageGenerator {
                         int pR = averageRed(p1, p2, p3, p4);
                         int pG = averageGreen(p1, p2, p3, p4);
                         int pB = averageBlue(p1, p2, p3, p4);
+                        */
+
+                        int pR = 0;
+                        int pG = 0;
+                        int pB = 0;
+
+                        for (int a=0; a<aa_amount; a++) {
+                            for (int b=0; b<aa_amount; b++) {
+                                int rgb = image.getRGB(i*aa_amount + a, j*aa_amount + b);
+                                pR += extractRed(rgb);
+                                pG += extractGreen(rgb);
+                                pB += extractBlue(rgb);
+                            }
+                        }
+
+                        pR /= aa_amount*aa_amount;
+                        pG /= aa_amount*aa_amount;
+                        pB /= aa_amount*aa_amount;
                         
                         int pixel = rgbToInt(pR,pG,pB);
                         buffer.setRGB(i, j, pixel);
@@ -125,50 +133,82 @@ public class ImageGenerator {
         shiftY = 0;
         antiAliasing = false;
         threshold = 64;
+        aa_amount = 2;
         
         x1 = -1; y1 = -1;
         x2 = 1; y2 = 1;
-        setDelta(0.01); // set height et width automatiquement
+        setstep(0.01); // set height et width automatiquement
+
+        setValueToColorDefaultFunction();
     }
 
     // setters
     public void setWidth(int width) {
         if (width<1) return;
-        x2 = x1 + width*delta;
+        double d = ((width-this.width)*step)/2;
+        x1-=d;
+        x2+=d;
         this.width = width;
     }
+
     public void setHeight(int height) {
         if (height<1) return;
-        y2 = y1 + height*delta;
+        double d = ((height-this.height)*step)/2;
+        y1-=d;
+        y2+=d;
         this.height = height;
     }
+
     public void setZoom(double zoom) {
         if (zoom>0) this.zoom = zoom;
     }
+
     public void setShiftX(double shiftX) {this.shiftX = shiftX;}
     public void setShiftY(double shiftY) {this.shiftY = shiftY;}
     public void setAntiAliasing(boolean antiAliasing) {this.antiAliasing = antiAliasing;}
     public void setPoint1(double x, double y) {
-        if (x > x2-delta) x1 = x2-delta;
+        if (x > x2-step) x1 = x2-step;
         else x1=x;
-        if (y > y2-delta) y1 = y2-delta;
+        if (y > y2-step) y1 = y2-step;
         else y1 = y;
-        width = (int)((x2-x1)/delta);
-        height = (int)((y2-y1)/delta);
+        width = (int)((x2-x1)/step);
+        height = (int)((y2-y1)/step);
     }
     public void setPoint2(double x, double y) {
-        if (x < x1+delta) x2 = x1+delta;
+        if (x < x1+step) x2 = x1+step;
         else x2 = x;
-        if (y < y1+delta) y2 = y1+delta;
+        if (y < y1+step) y2 = y1+step;
         else y2 = y;
-        width = (int)((x2-x1)/delta);
-        height = (int)((y2-y1)/delta);
+        width = (int)((x2-x1)/step);
+        height = (int)((y2-y1)/step);
     }
-    public void setDelta(double d) {
+    public void setstep(double d) {
         if (d <= 0) return;
-        delta = d;
+        step = d;
         width = (int)((x2 - x1) / d);
         height = (int)((y2 - y1) / d);
+    }
+    public void setAntiAliasingAmount(int amount) {
+        if (amount<2) return;
+        this.aa_amount = amount;
+    }
+    public void setValueToColorFunction(ThreeIntToInt function) {
+        valueToColor = function;
+    }
+    public void setValueToColorDefaultFunction() {
+        valueToColor = (int val, int min, int max) -> {
+            if (val<min || val>max) return 0x000000; //cas erreur
+            if (val==max) return 0;
+            return Color.HSBtoRGB((float)(val+min)/max, 0.8f, 0.7f);
+        };
+    }
+    public void setValueToColorDefaultFunction2() {
+        valueToColor = (int val, int min, int max) -> {
+            if (val>max || val<min) return 0xff0000; //cas erreur
+            if (val==max) return 0xaaccff;
+            val=255*(val+min)/max; 
+            return rgbToInt(val, val, val);
+        };
     }
 
     // getters
@@ -178,6 +218,7 @@ public class ImageGenerator {
     public double getShiftX() {return shiftX;}
     public double getShiftY() {return shiftY;}
     public boolean getAntiAliasing() {return antiAliasing;}
+    public int getAntiAliasingAmount() {return aa_amount;}
 
     private int rgbToInt(int r, int g, int b) {
         return (r << 16) | (g << 8) | b;
@@ -233,9 +274,9 @@ public class ImageGenerator {
 
         if (antiAliasing) {
             smol=new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-            height *=2;
-            width *=2;
-            delta /= 2;
+            height *= aa_amount;
+            width *= aa_amount;
+            step /= aa_amount;
         }
         
         image=new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
@@ -250,9 +291,9 @@ public class ImageGenerator {
         pool.invoke(work);        
 
         if (antiAliasing) {
-            height /=2;
-            width /=2;
-            delta *=2;
+            height /= aa_amount;
+            width /= aa_amount;
+            step *= aa_amount;
             threshold = height/threadsNb;
             RecursiveAction aawork = new AntiAliasingWork(0, height, smol);
             ForkJoinPool pool2 = new ForkJoinPool();
---
+++ src/ThreeIntToInt.java
@@ -0,0 +1,4 @@
+@FunctionalInterface
+interface ThreeIntToInt {
+    int apply(int value, int min, int max);
+}
