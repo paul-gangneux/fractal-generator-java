4e6dee0933dc1cd87b99053a98925c7218190f47
Olivier Moreau <m242@pm.me>
2021-12-15 17:06:14 +0100

    rebased from master to get formatter

---
+++ src/Cli.java
@@ -0,0 +1,37 @@
+import java.util.function.Function;
+
+public class Cli {
+	public void run(String ... args) {
+		ImageGenerator imgg = new ImageGenerator();
+		String output = "images/output.png";
+		boolean isMandelbrot = false;
+		for (String arg_tot: args) {
+			String[] sa= arg_tot.split("=");
+			String arg = sa[0];
+			switch (arg) {
+				case "--width": imgg.setWidth(Integer.parseInt(sa[1])); break;
+				case "--height": imgg.setHeight(Integer.parseInt(sa[1]));break;
+				case "--zoom": imgg.setZoom(Integer.parseInt(sa[1]));break;
+				case "--shiftx": imgg.setShiftX(Integer.parseInt(sa[1]));break;
+				case "--shifty": imgg.setShiftY(Integer.parseInt(sa[1]));break;
+				case "--output": output = sa[1]; break;
+				case "--mandelbrot": isMandelbrot = true; break;
+				case "--julia": break; //TODO
+				default: System.out.println(arg); usage(); break;
+			}
+		}
+
+		TwoDoublesToInt func;
+		if (isMandelbrot) {
+			func = new Mandelbrot();
+		} else {
+			Function<Complex,Complex> f = z -> z.multiply(z).add(new Complex(-0.729, 0.1889));
+			func = Julia.JuliaFactory(1000, 0, 2, f);
+		}
+		imgg.create(func, output);
+	}
+
+	private void usage() {
+		System.err.println("bad arg");
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 public class Main {
-
   public static void main(String[] args) {}
+    public static void main(String[] args) {
+        if (args.length > 0) {
+		new Cli().run(args);
+	}
+    }
 }
