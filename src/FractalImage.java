28a3bf670c66fd38b36f64fccf482bd293de1d87
Paul Gangneux <paul.v.gangneux@gmail.com>
2021-12-20 21:58:42 +0100

    big refactor

---
+++ src/FractalImage.java
@@ -0,0 +1,112 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import javax.swing.*;
+import javax.swing.event.MouseInputListener;
+
+public class FractalImage extends JLabel implements MouseInputListener {
+
+  private transient ImageGenerator ig;
+  private transient BufferedImage img;
+  private JFrame frame;
+
+  private ImageButtonPanel ipp;
+
+  private boolean calculating;
+
+  private int anchorX = 0, anchorY = 0;
+  private int newX = 0, newY = 0;
+
+  public FractalImage(ImageGenerator ig, JFrame frame) {
+    this.frame = frame;
+    this.ig = ig;
+    img = ig.getImage();
+    setIcon(new ImageIcon(img));
+    addMouseListener(this);
+    addMouseMotionListener(this);
+    setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+    calculating = false;
+  }
+
+  public void setImageButtonPanel(ImageButtonPanel imageButtonPanel) {
+    ipp = imageButtonPanel;
+  }
+
+  private void redraw() {
+    img = ig.getImageWithShift(newX - anchorX, newY - anchorY);
+    setIcon(new ImageIcon(img));
+  }
+
+  private void recalculateAfterShift() {
+    ig.setShiftX(ig.getShiftX() - (newX - anchorX) * ig.getZoom() * ig.getStep());
+    ig.setShiftY(ig.getShiftY() - (newY - anchorY) * ig.getZoom() * ig.getStep());
+    recalculate();
+    anchorX = 0;
+    anchorY = 0;
+    newX = 0;
+    newY = 0;
+  }
+
+  public void recalculate() {
+    if (calculating) return;
+    calculating = true;
+    ig.applyShift();
+    ig.applyZoom();
+    ipp.updateValues();
+    frame.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+    setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+    ig.generateBuffer();
+    frame.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+    setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+    img = ig.getImage();
+    setIcon(new ImageIcon(img));
+    calculating = false;
+    frame.pack();
+    // thisJframe.setLocationRelativeTo(null);
+  }
+
+  @Override
+  public void mouseClicked(MouseEvent e) {
+    // nothing
+  }
+
+  @Override
+  public void mouseEntered(MouseEvent e) {
+    // nothing
+  }
+
+  @Override
+  public void mouseExited(MouseEvent e) {
+    // nothing
+  }
+
+  @Override
+  public void mousePressed(MouseEvent e) {
+    if (calculating) return;
+    setCursor(Cursor.getPredefinedCursor(Cursor.MOVE_CURSOR));
+    anchorX = e.getX();
+    anchorY = e.getY();
+    newX = e.getX();
+    newY = e.getY();
+  }
+
+  @Override
+  public void mouseReleased(MouseEvent e) {
+    if (calculating) return;
+    recalculateAfterShift();
+    setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+  }
+
+  @Override
+  public void mouseDragged(MouseEvent e) {
+    if (calculating) return;
+    newX = e.getX();
+    newY = e.getY();
+    redraw();
+  }
+
+  @Override
+  public void mouseMoved(MouseEvent e) {
+    // nothing
+  }
+}
